271aa89e8f8f5e50698a4ad74f7b9607d37bcb51
KING <abayemiracle@gmail.com>
2020-10-11 12:38:55 -0700

    Updates

---
+++ precache-manifest.e813302d00512525a78c0d6fc37017cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fef22f40223f3461b2e563321ec1cfa",
+    "url": "/live-quote/index.html"
+  },
+  {
+    "revision": "28f0e8cf06c985e6cd87",
+    "url": "/live-quote/static/css/main.c1c11922.chunk.css"
+  },
+  {
+    "revision": "25b98476962ddb3f0af8",
+    "url": "/live-quote/static/js/2.097ec909.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/live-quote/static/js/2.097ec909.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28f0e8cf06c985e6cd87",
+    "url": "/live-quote/static/js/main.f0edd640.chunk.js"
+  },
+  {
+    "revision": "3f9abf1c1f4e6a15fd0e",
+    "url": "/live-quote/static/js/runtime-main.72afce93.js"
+  }
+]);
